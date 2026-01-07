575e1d20b7d72833af8362905be563e8c8785dcc
GitHub Action <action@github.com>
2026-01-07 12:42:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/tomatos03/tabnumber/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tomatos03/tabnumber/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomatos03.tabnumber.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomatos03.tabnumber.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
